076204376441fc45877ac77684c263e84f5a47ba
Hoang Van Binh <hoangvanbinh141205@gmail.com>
2025-11-09 15:15:33 +0700

    fix done fe-5

--- client/src/layouts/AppLayout.tsx
+++ client/src/layouts/AppLayout.tsx
@@ -1,5 +1,7 @@
-import { Suspense } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '@/components/ErrorFallback';
 
 // import Sidebar from '../components/Sidebar'
 // import Header from '../components/Header'
@@ -7,17 +9,20 @@ import { Outlet } from 'react-router-dom'
 const LoadingFallback = () => <div>Đang tải trang...</div>;
 
 const AppLayout = () => {
-    return (
-        <div className="app-layout">
-            {/* <Sidebar /> */}
-            <main className="main-content">
-                {/* <Header /> */}
-                <Suspense fallback={<LoadingFallback />}>
-                    <Outlet />
-                </Suspense>
-            </main>
-        </div>
-    );
+  return (
+    <div className="app-container">
+      {/* <Sidebar /> */}
+      <main className="main-content">
+        {/* <Header /> */}
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<LoadingFallback />}>
+            <Outlet />
+          </Suspense>
+        </ErrorBoundary>
+      </main>
+    </div>
+  );
 };
+//Done
 
 export default AppLayout;
