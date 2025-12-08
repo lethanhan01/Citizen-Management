d0c7d93d75684cc3efa71de25162310b7e0fda44
nhithedev <nhitofu@gmail.com>
2025-12-09 02:05:12 +0700

    test UI login voi css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')!).render(
         <RouterProvider router={router} />
       </ThemeProvider>
 
-      <ReactQueryDevtools initialIsOpen={false} />
+
     </QueryClientProvider>
   </StrictMode>,
 );
