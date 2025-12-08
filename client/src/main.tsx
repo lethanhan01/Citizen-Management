eb0ff6209831f788af39e8a15db7402daabbe5c2
nhithedev <nhitofu@gmail.com>
2025-12-09 02:10:54 +0700

    xoa import ko dung

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import './index.css';
 import router from './routes/index.tsx';
